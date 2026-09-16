660aeaef7d6b8ee19bf57225b4b64d561dd7a756
Waraporn.SWSK <deerwaraporn2@gmail.com>
2026-09-16 10:24:47 +0200

    Set up HomePage and React Router

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+function HomePage() {
+    return (
+        <main>
+            <h1>Welcome to Tast Of Thai</h1>
+            <p>Discover delicious Thai recipes</p>
+        </main>
+    );
+}
+
+export default HomePage;
