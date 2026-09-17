16cbf7c249a2c7474d3c875a9761c402c872c71a
Waraporn.SWSK <deerwaraporn2@gmail.com>
2026-09-17 16:13:28 +0200

    Add footer and responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FavoritesPage from "./pages/FavoritesPage";
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 import "./App.css";
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/favorites" element={<FavoritesPage />} />
       </Routes>
-    
+
+      <Footer />
+  
     
     </BrowserRouter>
     
---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+function Footer(){
+
+    return (
+        <footer className="site-footer">
+            <div className="footer-content">
+                <h2>Taste Of Thai</h2>
+
+                <p>Authentic Thai Flavors & recipes</p>
+
+                <nav>
+                    <a href="/">Home</a>
+                    <a href="/favorites">Favorites</a>
+                </nav>
+
+                <p className="copyright">
+                    © 2026 Taste Of Thai
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
