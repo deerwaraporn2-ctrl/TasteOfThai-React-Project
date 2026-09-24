7e3dd60ba27e1f34ec41345c3c71385281f350e5
Waraporn.SWSK <deerwaraporn2@gmail.com>
2026-09-24 10:49:51 +0200

    Add favorites localStorage

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,10 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const FavoritesContext = createContext();
 
 export function FavoritesProvider({ children }) {
-  const [favorites, setFavorites] = useState([]);
 
+ const [favorites, setFavorites] = useState(() => {
+  const savedFavorites = localStorage.getItem("favorites");
+
+  return savedFavorites ? JSON.parse(savedFavorites) : [];
+});
+
+useEffect(() => {
+  localStorage.setItem("favorites", JSON.stringify(favorites));
+}, [favorites]);
+  
   function toggleFavorite(recipe) {
     const isFavorite = favorites.some(
       (favorite) => favorite.idMeal === recipe.idMeal
